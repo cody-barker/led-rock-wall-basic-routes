ac09093e77de53d4c3981cebee08003b8b21757c
Cody <codybarker.or@gmail.com>
2024-01-13 12:26:30 -0800

    create initial arduino code with one easy route

---
+++ led-rock-wall/led-rock-wall.ino
@@ -0,0 +1,23 @@
+#include <FastLED.h>
+
+#define LED_PIN 7
+#define NUM_LEDS 20
+
+CRGB leds[NUM_LEDS];
+
+int easy[8] = {2, 5, 7, 9, 13, 15, 18, 20}
+
+void setup() {
+
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+
+}
+
+void loop() {
+
+  for (int i : easy) {
+    leds[i] = CRGB(0, 0, 255);
+    FastLED.show();
+    delay(500);
+  }
+}
