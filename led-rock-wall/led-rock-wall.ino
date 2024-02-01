29161ce8c8b6188a967f696a62e6787985a01569
Cody <codybarker.or@gmail.com>
2024-01-31 16:42:44 -0800

    add hard route

--- led-rock-wall/led-rock-wall.ino
+++ led-rock-wall/led-rock-wall.ino
@@ -7,6 +7,8 @@ CRGB leds[NUM_LEDS];
 
 // Your Custom Routes //
 int easy[8] = {2, 5, 7, 9, 13, 15, 18, 20};
+int medium[5] = {0, 3, 6, 8, 10};
+int hard[4] = {1, 5, 10, 19};
 
 void setup() {
 
@@ -17,10 +19,9 @@ void setup() {
 void loop() {
 // Change Routes by Changing the Name in the For Loop //
 // Change CRGB value to change color. Keep 2 values 0 for safety //
-  for (int i : easy) {
+  for (int i : hard) {
     leds[i] = CRGB(0, 0, 255);
     FastLED.show();
     delay(500);
   }
 }
-
